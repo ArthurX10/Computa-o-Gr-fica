244e3147565243ef85b5e8a914f4fde1b276aa2f
viniciusibiapina007 <viniciusaluno2020@gmail.com>
2026-03-19 17:54:08 -0300

    Update ModuloEscala.py

--- ModuloEscala.py
+++ ModuloEscala.py
@@ -1,26 +1,7 @@
-
-def caucula_pivo():
-    pass
-
-'''
-def muda_escala(ponto, fatorX, fatorY):
-    ponto[0] *= fatorX
-    ponto[1] *= fatorY
-    print(ponto)
-
-muda_escala([2,5], 8, 9)
-
-pontos = [
-    (3,4), (6,4), (5,8), (4,8),
-    (9,4), (12,4), (11,8), (10,8),
-    (15,4), (18,4), (17,8), (16,8)
-]
-'''
-
-def escalada(figura, fatorX, fatorY):
-    nova_figura = []
-    for (x,y) in figura:
-        nova_figura.append((x*fatorX, y*fatorY))
-    print(nova_figura)
+def muda_escala(fatX, fatY):
     
-#escalada(pontos, 8, 9)
+    #global pontos
+    
+    for i in range(0,12):
+        temp = pontos[i]
+        pontos[i] = (temp[0]*fatX , temp[1]*fatY)
